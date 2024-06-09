8ee1bbea81046095b1bd88255e584be2662165cc
nnvi <vinnie.felim@gmail.com>
2024-06-09 12:44:26 +0700

    done search log

--- controllers/log_activityController.js
+++ controllers/log_activityController.js
@@ -1,4 +1,4 @@
-const { where } = require('sequelize')
+const { where, Op } = require('sequelize')
 const {log_activity,users}= require('../models')
 
 class LogActivityController{
@@ -9,16 +9,36 @@ class LogActivityController{
             const pageSize = 5
             const start = (page-1)*pageSize
             const end = page*pageSize
+            const search = req.query.search || ''
+
+            const checkDate = /^\d{4}-\d{2}-\d{2}$/;
 
             const countLog = await log_activity.count()
             const totalPage = (countLog % pageSize != 0? (Math.floor(countLog/pageSize))+1: countLog/pageSize)
+            let whereClause = {};
+            if (checkDate.test(search)) {
+                // If search is a valid date, filter by date
+                whereClause = {
+                    createdAt: {
+                        [Op.between]: [new Date(search), new Date(new Date(search).getTime() + 86400000)] // Assuming search represents a single day
+                    }
+                };
+            } else {
+                // If search is not a valid date, assume it's a user name and filter by user name
+                whereClause = {
+                    '$user.name$': {
+                        [Op.like]: `%${search}%`
+                    }
+                };
+            }
 
             const getAllLogActivity=await log_activity.findAll({
                 attributes:['id','user_id','activity_info'],
                 include:{
                     model: users,
                     attributes:['name']
-                }
+                },
+                where:whereClause
             })
             const paginationLog = getAllLogActivity.slice(start,end)
 
@@ -30,7 +50,7 @@ class LogActivityController{
             })
         }
         catch(err){
-            res.status(500).json({message:err})
+            res.status(500).json({message:err.message})
         }
     }
 }
--- middlewares/cloudinary.js
+++
@@ -1,8 +0,0 @@
-const cloudinary= require('cloudinary').v2
-
-cloudinary.config({ 
-    cloud_name: "dktcfw5wm", 
-    api_key: "182555149122433", 
-    api_secret: "jQQTh_VOcv-__5EipOvSNKj9fGI"
-});
-module.exports= cloudinary
