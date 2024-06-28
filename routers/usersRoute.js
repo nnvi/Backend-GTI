cc0411905c8462a99470fc21dac41403d2df880a
nnvi <vinnie.felim@gmail.com>
2024-06-28 21:12:43 +0700

    contAuthorization revise

--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {addUser, getUser, getUserbyUuid, deleteUser, EditUser, login, currentUser} = require('../controllers/userController')
+const {addUser, getUser, getUserbyUuid, deleteUser, EditUser} = require('../controllers/userController')
 const {userAuthorization} = require('../middlewares/authorization')
 const upload = require('../middlewares/multer')
 
